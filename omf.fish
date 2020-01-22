2a24a456d9acf3e2e99fb55b2b8fe13aab10ccc8
Julien Grenier <j.grenier@samsung.com>
2020-01-22 09:43:43 -0500

    Add virtualfish to omf

--- omf.fish
+++ omf.fish
@@ -1,3 +1,4 @@
 curl -L https://get.oh-my.fish | fish
 ln -s ~/dotfiles/bundle.omf  $HOME/.comfig/omf/bundle
 ln -s ~/dotfiles/theme.omf  $HOME/.comfig/omf/theme
+ln -s ~/dotfiles/config.fish $HOME/.config/fish/config.fish
