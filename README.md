ff95e31e58e279187e6ee22121e43922a515c7f6
Julien Grenier <j.grenier@samsung.com>
2020-01-22 09:42:49 -0500

    Fix command in README

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Repository the keep my dot files in sync
 
-> ln -s -T ~/dotfiles/.dotfile ~/.dotfile
+> ln -s  ~/dotfiles/.dotfile ~/.dotfile
---
+++ omf.fish
@@ -0,0 +1,3 @@
+curl -L https://get.oh-my.fish | fish
+ln -s ~/dotfiles/bundle.omf  $HOME/.comfig/omf/bundle
+ln -s ~/dotfiles/theme.omf  $HOME/.comfig/omf/theme
